77dde34fbb8e1ba13ef8c91a06647ab35aeb3866
Sai Dattaram More <168020850+saimore1234@users.noreply.github.com>
2026-02-16 17:54:52 +0530

    Commit1

--- accounting_dashboard/accounting_dashboard/report/custom_items/custom_items.js
+++ accounting_dashboard/accounting_dashboard/report/custom_items/custom_items.js
@@ -1,5 +1,79 @@
+// frappe.query_reports["custom items"] = {
+//     filters: [
+//         {
+//             fieldname: "item_code",
+//             label: "Item Code",
+//             fieldtype: "Link",
+//             options: "Item"
+//         },
+//         {
+//             fieldname: "roll_no",
+//             label: "Roll No",
+//             fieldtype: "Data"
+//         },
+//         {
+//             fieldname: "rack",
+//             label: "Rack",
+//             fieldtype: "Data"
+//         },
+//         {
+//             fieldname: "colour_code",
+//             label: "Colour Code",
+//             fieldtype: "Data"
+//         },
+//         {
+//             fieldname: "weight_gsm",
+//             label: "Weight GSM",
+//             fieldtype: "Float"
+//         },
+//         {
+//             fieldname: "width",
+//             label: "Width",
+//             fieldtype: "Float"
+//         },
+//         {
+//             fieldname: "net_length",
+//             label: "Net Length",
+//             fieldtype: "Float"
+//         },
+//         {
+//             fieldname: "from_date",
+//             label: "From Date",
+//             fieldtype: "Date"
+//         },
+//         {
+//             fieldname: "to_date",
+//             label: "To Date",
+//             fieldtype: "Date"
+//         }
+//     ],
+
+//     formatter: function(value, row, column, data, default_formatter) {
+//         value = default_formatter(value, row, column, data);
+
+//         if (data && data.item_code === "GRAND TOTAL") {
+//             value = `<b style="color:green">${value}</b>`;
+//         }
+
+//         return value;
+//     }
+// };
+
 frappe.query_reports["custom items"] = {
     filters: [
+        {
+            fieldname: "group_by",
+            label: "Group By",
+            fieldtype: "Select",
+            options: [
+                "",
+                "Item Code",
+                "Rack",
+                "Colour Code",
+                "Weight GSM",
+                "Width"
+            ]
+        },
         {
             fieldname: "item_code",
             label: "Item Code",
