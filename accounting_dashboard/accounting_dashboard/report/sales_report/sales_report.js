5c7b8c3c3779acffcdfea96620810568dc53c4a9
saimore1234 <moresai881@gmai.com>
2026-04-30 10:51:11 +0530

    Initial commit2

--- accounting_dashboard/accounting_dashboard/report/sales_report/sales_report.js
+++ accounting_dashboard/accounting_dashboard/report/sales_report/sales_report.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 
-frappe.query_reports["Sales Reporting"] = {
+frappe.query_reports["Sales Report"] = {
     "filters": [
         {
             "fieldname": "from_date",
